d131b0b6803bc230c626216aca0667d4950b8ca6
Mateus Carnaúba <mateuscarnauba1@gmail.com>
2025-11-10 15:27:50 -0300

    adicionei a logo na tela de login

--- src/login.php
+++ src/login.php
@@ -63,6 +63,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login - Encontre o Campo</title>
+    <link rel="shortcut icon" href="../img/Logo - Copia.jpg" type="image/x-icon">
     <link rel="stylesheet" href="css/login.css">
 </head>
 <body>

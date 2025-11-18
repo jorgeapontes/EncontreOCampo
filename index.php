2ad920eda084bb565c3ec1ff356f8fb09ea14d01
Rafael <rafaeltonetti.cardoso@gmail.com>
2025-11-18 12:12:21 -0300

    cores

--- index.php
+++ index.php
@@ -23,7 +23,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Encontre o Campo - E-commerce de Frutas</title>
     <link rel="stylesheet" href="index.css">
-    <link rel="shortcut icon" href="img/Logo - Copia.jpg" type="image/x-icon">
+    <link rel="shortcut icon" href="img/logo-nova.png" type="image/x-icon">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Zalando+Sans+SemiExpanded:ital,wght@0,200..900;1,200..900&display=swap" rel="stylesheet">

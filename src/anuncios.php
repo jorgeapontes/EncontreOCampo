5da92ce681fc936849905b1090204fa13144cb5e
Rafael <rafaeltonetti.cardoso@gmail.com>
2025-12-01 11:43:06 -0300

    Design comprador

--- src/anuncios.php
+++ src/anuncios.php
@@ -127,7 +127,6 @@ try {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Anúncios - Encontre Ocampo</title>
-    <link rel="stylesheet" href="../index.css"> 
     <link rel="stylesheet" href="css/anuncios.css">
     <link rel="shortcut icon" href="../img/logo-nova.png" type="image/x-icon">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
